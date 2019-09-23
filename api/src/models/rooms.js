0a7cef75a8c83ec369260aeb965b63db66bd709d
dlaurentn <dlaurent@student.42.fr>
2019-09-23 14:50:48 +0200

    [back/feature] creation of models for equipments, reservations, rooms and models [issue #4]

---
+++ api/src/models/rooms.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema, model } from 'mongoose';
+import validator from 'validator';
+
+/*
+  Rooms model contains all rooms as described in json input file.
+  For flexibility reasons, equipments are defined in another model.
+*/
+
+const RoomsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    validate: value => validator.isLength(value, { min: 2, max: 15 }) && validator.isAscii(value),
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    validate: value => validator.isLength(value, { min: 2, max: 255 }) && validator.isAscii(value),
+  },
+  capacity: {
+    type: Number,
+    required: true,
+    validate: value => validator.isInt(value, { min: 1, max: 99 }),
+  },
+  equipments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Equipments',
+  }],
+  createdAt: Date,
+  updatedAt: Date,
+});
+
+RoomsSchema.pre('update', function () {
+  this.updatedAt = Date.now();
+});
+
+RoomsSchema.pre('findOneAndUpdate', function () {
+  this.updatedAt = Date.now();
+});
+
+RoomsSchema.pre('save', function (next) {
+  if (!this.createdAt) this.createdAt = Date.now();
+  this.updatedAt = Date.now();
+  next();
+});
+
+export default model('Rooms', RoomsSchema, 'rooms');
